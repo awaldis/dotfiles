b4de2beda59b03511df090c913116fbee46afc38
Andrew Waldis <andrew@andrewwaldis.com>
2025-11-19 17:20:54 -0500

    Update PowerShell profile to print startup time

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,7 @@ oh-my-posh init pwsh --config 'C:\Users\awald\source\repos\dotfiles\oh-my-posh\m
 # PSReadLine configuration for history search
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Print the current date and time on startup
+$tz = (Get-TimeZone).Id
+Write-Host "Shell started at: $(Get-Date -Format 'ddd MMM dd HH:mm:ss') $tz $(Get-Date -Format 'yyyy')" -ForegroundColor Cyan
